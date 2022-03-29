226b958ae22581998edddeef8c8b940febab4edd
Shubhangi meshram <101330420+9689725619@users.noreply.github.com>
2022-03-29 17:38:28 +0530

    Add files via upload

---
+++ wap to print fibonacci series using for loop.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int f0=0,f1=1,f2,n,i;
+	printf("enter number :");
+	scanf("%d",&n);
+	for(i=3; i<=n; i++){
+		
+		f2=f0+f1;
+		printf("%d\t",f2);
+		f2=f1;
+		f1=f2;
+	}
